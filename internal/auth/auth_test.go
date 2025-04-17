20516dc56555500bebc9cfd0ea4d3e7e04c73492
ProjectCaffeine <smith_eg21@yahoo.com>
2025-04-17 16:57:35 -0500

    Added tests

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,16 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestNoHeader(t *testing.T) {
+	header := http.Header{}
+
+	_, err := GetAPIKey(header)
+
+	if err.Error() == "no authorization header included" {
+		t.Fatal("No error received!")
+	}
+}
